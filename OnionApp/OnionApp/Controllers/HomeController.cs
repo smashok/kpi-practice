cd0d75d5cefb090c8e0f19427f984691bc64b0e8
smashok <anteluk@icloud.com>
2021-06-23 15:33:27 +0300

    OnionApp
    /

---
+++ OnionApp/OnionApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnionApp.Domain.Core;
+using OnionApp.Domain.Interfaces;
+using OnionApp.Services.Interfaces;
+
+namespace OnionApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPhoneRepository repo;
+        IOrder order;
+        public HomeController(IPhoneRepository r, IOrder o)
+        {
+            repo = r;
+            order = o;
+        }
+        public ActionResult Index()
+        {
+            var phones = repo.GetPhoneList();
+            return View(phones);
+        }
+
+        public ActionResult Buy(int id)
+        {
+            Phone phone = repo.GetPhone(id);
+            order.MakeOrder(phone);
+            return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            repo.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
